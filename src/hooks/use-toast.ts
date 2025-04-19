20436afdc33153c15094f0d24c072902d91c145e
meowrescue <patrick@meowrescue.org>
2025-04-19 02:46:29 -0400

    Update use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,5 +1,2 @@
-
-import { useToast as usePrimitiveToast, toast as primitiveToast } from "./use-toast-primitive";
-
-export const useToast = usePrimitiveToast;
-export const toast = primitiveToast;
+import { toast } from "sonner";
+export { toast };
